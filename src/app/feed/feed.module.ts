8b2c59c6808f1106b7b9ca48a4dcbe354ffa4b0e
Chandu <chandu@Chandus-MacBook-Pro.local>
2021-01-29 19:21:45 -0500

    Edited CSS

--- src/app/feed/feed.module.ts
+++ src/app/feed/feed.module.ts
@@ -3,10 +3,10 @@ import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Tab2Page } from './tab2.page';
+import { FeedPage } from './feed.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
-import { Tab2PageRoutingModule } from './tab2-routing.module';
+import { FeedPageRoutingModule } from './feed-routing.module';
 
 @NgModule({
   imports: [
@@ -14,8 +14,8 @@ import { Tab2PageRoutingModule } from './tab2-routing.module';
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab2PageRoutingModule
+    FeedPageRoutingModule
   ],
-  declarations: [Tab2Page]
+  declarations: [FeedPage]
 })
-export class Tab2PageModule {}
+export class FeedPageModule {}
